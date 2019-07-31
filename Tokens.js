e5c3008501096f9c5835406460f2920835af801d
Juan Zabala <juanpablozabala21@gmail.com>
2019-07-31 09:48:20 -0400

    Add files via upload

---
+++ Tokens.js
@@ -0,0 +1,67 @@
+'use strict';
+const Lex = require('lexical-parser')
+let input = 'block { int a = 0 int b = 3 if (a < b && b > a) { print("Hello World") } }'
+// You can specify an exact string or a regex for the token
+let tokenMatchers = [
+    'block',
+    'if',
+    'break',
+    'or',
+    'def',
+    'del',
+    'else', 
+    'elif',
+    'not', 
+    'continue',
+    'in',
+    'try',
+    'exec', 
+    'print', 
+    'raise', 
+    'lambda',
+    'class',
+    'import',
+    'yield',
+    'assert',
+    'except',
+    'for', 
+    '+',
+    '-',
+    '*',
+    '**',
+    '%', 
+    '/',
+    '//',
+    '{',
+    '}',
+    '(',
+    ')',
+    '<',
+    '>',
+    '=',
+    '&&',
+    'int',
+    ['integer', /[0-9]+/],
+    ['id', /[a-zA-Z][a-zA-Z0-9]*/],
+    ['string', /\".*?\"/]
+]
+// The pattern to ignore in the input
+let ignorePattern = '[\n\s \t]+'
+let lex = new Lex(input, tokenMatchers, ignorePattern)
+let token = undefined
+try {
+    do {
+        token = lex.nextToken()
+        console.log(token)
+    } while (token)
+} catch (err) {
+    // Error handling
+    if (err.code === "LEXICAL_ERROR") {
+        console.log(`\n${err.message}\n`)
+        console.log(`Position: ${err.position}`)
+        console.log(`Character: ${err.character}`)
+        console.log(`Nearby code: ${err.nearbyCode}`)
+    }
+    else
+        console.log(err)
+}
